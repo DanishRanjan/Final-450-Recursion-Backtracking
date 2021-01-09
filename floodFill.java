a713fcd3984a72f7eb7d9f63193a580732a63f3a
Danish Ranjan <ranjandanish@gmail.com>
2021-01-10 02:25:10 +0530

    Floor Fill

--- floodFill.java
+++ floodFill.java
@@ -1,7 +1,37 @@
 import java.util.*;
 public class floodFill {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int[][] arr = new int[n][m];
+		
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr[0].length;j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		
+		
+		boolean[][] visited = new boolean[n][m];
+		paths(arr, 0, 0, "", visited);
+		
+	}
+	public static void paths(int[][] maze, int row, int col, String ans, boolean[][] visited) {
+		if(row<0 || col<0 || row==maze.length || col==maze[0].length || maze[row][col]==1 || visited[row][col]==true) {
+			 return; 
+		}
+		if(row==maze.length-1 && col==maze[0].length-1) {
+			System.out.println(ans);
+			return;
+		}
+		
+		visited[row][col] = true;
+		paths(maze, row - 1, col, ans + "t", visited);
+		paths(maze, row, col - 1, ans + "l", visited);
+		paths(maze, row + 1, col, ans + "d", visited);
+		paths(maze, row, col + 1, ans + "r", visited);
+		visited[row][col] = false;
 		
 	}
 
